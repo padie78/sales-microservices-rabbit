cbd56a7aab4f42b5b62fc8590ad824dbd2c70de9
Diego Liascovich <padie78@gmail.com>
2025-06-11 19:13:34 +0300

    adding dockerfiles and package.json files

---
+++ billings/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+// import { OrderController } from './order.controller';
+// import { OrderCreatedPublisher } from './application/order-created.publisher';
+// import { CreateOrderUseCase } from './application/create-order.use-case';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ billings/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://rabbitmq:5672'],
+      queue: 'orders_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+}
+bootstrap();
